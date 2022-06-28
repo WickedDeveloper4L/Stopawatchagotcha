b9f204bb0fc2cce985e2bf52f34e9ed673e3f463
WickedDeveloper4L <101099456+WickedDeveloper4L@users.noreply.github.com>
2022-06-28 14:36:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+
+	alert("Heyy I'm chris! send me money to GTBank 0249110235.. lol")
+	const time_el = document.querySelector('.watch .time');
+	const start_btn = document.getElementById('start');
+	const stop_btn = document.getElementById("stop");
+	const reset_btn = document.getElementById("reset");
+
+
+
+	let seconds = 0;
+	let interval = null;
+
+	start_btn.addEventListener('click', start);
+	stop_btn.addEventListener('click', stop);
+	reset_btn.addEventListener('click', reset);
+
+
+	function timer() {
+		seconds++;
+
+		//Format our time
+
+		let hrs = Math.floor(seconds / 3600);
+		let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+		let secs = seconds % 60;
+
+		// Making 0 to be before the time less than 10
+
+		if (secs < 10) secs = "0" + secs;
+		if (mins < 10) mins = "0" + mins;
+		if (hrs < 10) hrs = "0" + hrs;
+
+		time_el.innerText = `${hrs}:${mins}:${secs}`;
+	}
+
+	function start() {
+		if (interval) {
+			return
+		}
+
+		interval = setInterval(timer, 1000); 
+	}
+
+	function stop() {
+		clearInterval(interval);
+		interval = null;
+	}
+
+	function reset() {
+		stop();
+		seconds = 0;
+		time_el.innerText = '00:00:00';
+	}
+		
+}
+
+
+
+
+
+
+
+
+
